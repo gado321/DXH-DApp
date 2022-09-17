ea3c22da64e0ea55de2d3d13fd52cbc0f2082f4f
Hieu V <votruongtrunghieu@gmail.com>
2022-09-17 10:47:29 +0700

    Update smart contract and .gitignore

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -6,67 +6,71 @@
  *
  */
 
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
-
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
-
-// Define the contract structure
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    message: String,
-}
-
-// Define the default, which automatically initializes the contract
-impl Default for Contract{
-    fn default() -> Self{
-        Self{message: DEFAULT_MESSAGE.to_string()}
-    }
-}
-
-// Implement the contract structure
-#[near_bindgen]
-impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
-    }
-
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        // Use env::log to record logs permanently to the blockchain!
-        log!("Saving greeting {}", message);
-        self.message = message;
-    }
-}
-
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
-    }
-}
+ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+ use near_sdk::{log, near_bindgen};
+ 
+ // Define the default candidate
+ const DEFAULT_CANDIDATE: &str = &"DXH";
+ 
+ // Define the contract structure
+ #[near_bindgen]
+ #[derive(BorshDeserialize, BorshSerialize)]
+ pub struct Contract {
+     candidates: Vec<String>
+ }
+ 
+ // Define the default, which automatically initializes the contract
+ impl Default for Contract {
+     fn default() -> Self{
+         Self{candidates: vec![DEFAULT_CANDIDATE.to_string()]}
+     }
+ }
+ 
+ // Implement the contract structure
+ impl Contract {
+     fn set_candidates(&mut self, candidate: String) {
+         // Verify json structure
+ 
+         //
+         self.candidates.push(candidate);
+     }
+ 
+     fn get_candidates(&mut self) -> Vec<String> {
+         self.candidates.clone()
+     }
+     
+     fn remove_candidate(&mut self, candidate: String) {
+         self.candidates.retain(|x| * x != candidate);
+     }
+     
+ }
+ 
+ 
+ /*
+  * The rest of this file holds the inline tests for the code above
+  * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+  */
+ #[cfg(test)]
+ mod tests {
+     use super::*;
+ 
+     #[test]
+     fn get_candidates_testing() {
+         let mut contract = Contract::default();
+         assert_eq!(
+             contract.get_candidates()[0],
+             DEFAULT_CANDIDATE
+         );
+     }
+     #[test]
+     fn set_candidates_testing() {
+         let mut contract = Contract::default();
+         let tmp: String = "dxh".to_owned();
+         contract.set_candidates(tmp);
+         assert_eq!(
+             contract.get_candidates()[1],
+             "dxh".to_owned()
+         );
+     }
+ }
+ 
