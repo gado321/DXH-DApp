487f1c2cf3f29ea62f50c3bab735264c3a3d27e5
Hieu V <votruongtrunghieu@gmail.com>
2022-09-17 22:11:11 +0700

    Update smart contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,76 +1,97 @@
-/*
- * Example smart contract written in RUST
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://near-docs.io/develop/Contract
- *
- */
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{log, near_bindgen, env, AccountId};
+use json::{parse};
 
- use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
- use near_sdk::{log, near_bindgen};
- 
- // Define the default candidate
- const DEFAULT_CANDIDATE: &str = &"DXH";
- 
- // Define the contract structure
- #[near_bindgen]
- #[derive(BorshDeserialize, BorshSerialize)]
- pub struct Contract {
-     candidates: Vec<String>
- }
- 
- // Define the default, which automatically initializes the contract
- impl Default for Contract {
-     fn default() -> Self{
-         Self{candidates: vec![DEFAULT_CANDIDATE.to_string()]}
-     }
- }
- 
- // Implement the contract structure
- impl Contract {
-     fn set_candidates(&mut self, candidate: String) {
-         // Verify json structure
- 
-         //
-         self.candidates.push(candidate);
-     }
- 
-     fn get_candidates(&mut self) -> Vec<String> {
-         self.candidates.clone()
-     }
-     
-     fn remove_candidate(&mut self, candidate: String) {
-         self.candidates.retain(|x| * x != candidate);
-     }
-     
- }
- 
- 
- /*
-  * The rest of this file holds the inline tests for the code above
-  * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
-  */
- #[cfg(test)]
- mod tests {
-     use super::*;
- 
-     #[test]
-     fn get_candidates_testing() {
-         let mut contract = Contract::default();
-         assert_eq!(
-             contract.get_candidates()[0],
-             DEFAULT_CANDIDATE
-         );
-     }
-     #[test]
-     fn set_candidates_testing() {
-         let mut contract = Contract::default();
-         let tmp: String = "dxh".to_owned();
-         contract.set_candidates(tmp);
-         assert_eq!(
-             contract.get_candidates()[1],
-             "dxh".to_owned()
-         );
-     }
- }
- 
+// Define the contract structure
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    candidates: Vec<String>,
+    verified_candidates: Vec<String>
+}
+
+// Define the default, which automatically initializes the contract
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            candidates: vec![],
+            verified_candidates: vec![]
+        }
+    }
+}
+
+// Implement the contract structure
+#[near_bindgen]
+impl Contract {
+    // candidates
+    pub fn set_candidate(&mut self, candidate: String) {
+        parse(&candidate).expect("Wrong format!");
+
+        self.candidates.push(candidate);
+    }
+    
+    pub fn get_candidates(&mut self) -> Vec<String> {
+        self.candidates.clone()
+    }
+
+    pub fn remove_candidate(&mut self, candidate: String) {
+        self.candidates.retain(|x| * x != candidate);
+    }
+    
+
+    // verified candidates
+    pub fn set_verified_candidate(&mut self, candidate: String) {
+        let account_id = env::signer_account_id();
+        
+        if account_id.to_string() == "upi05.testnet".to_string() {
+            // Set MAX_COIN before pushing
+            self.verified_candidates.push(candidate.clone());
+            self.remove_candidate(candidate);
+
+        }        
+    }
+    
+    pub fn get_verified_candidates(&mut self) -> Vec<String> {
+        self.verified_candidates.clone()
+    }
+
+    pub fn remove_verified_candidate(&mut self, candidate: String) {
+        let account_id = env::signer_account_id();
+        if account_id.to_string() == "upi05.testnet".to_string() {
+            self.verified_candidates.retain(|x| * x != candidate);
+        }
+    }
+}
+
+
+// Unit test
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_and_set_candidates_testing() {
+        let mut contract = Contract::default();
+        let candidate: String = r#"
+
+        {
+            "code": 200,
+            "success": true,
+            "payload": {
+                "features": [
+                    "awesome",
+                    "easyAPI",
+                    "lowLearningCurve"
+                ]
+            }
+        }
+        
+        "#.to_owned();
+        contract.set_candidate(candidate.clone());
+        assert_eq!(
+            contract.get_candidates()[0],
+            candidate
+        );
+    }
+    
+}
